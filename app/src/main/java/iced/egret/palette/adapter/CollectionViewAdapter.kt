56306fb115c0f52c31558d960f2046c724b76c75
anticobalt <anticobalt@users.noreply.github.com>
2019-06-21 12:57:21 -0600

    Remove unused selection functions from sectioned adapters

--- app/src/main/java/iced/egret/palette/adapter/CollectionViewAdapter.kt
+++ app/src/main/java/iced/egret/palette/adapter/CollectionViewAdapter.kt
@@ -1,9 +1,7 @@
 package iced.egret.palette.adapter
 
-import androidx.recyclerview.widget.RecyclerView
 import iced.egret.palette.model.Coverable
 import iced.egret.palette.recyclerview_component.CollectionViewSection
-import iced.egret.palette.recyclerview_component.CoverViewHolder
 import iced.egret.palette.util.CollectionManager
 import io.github.luizgrp.sectionedrecyclerviewadapter.Section
 
@@ -11,20 +9,6 @@ class CollectionViewAdapter(contents: MutableList<Coverable>) : CoverableAdapter
 
     private var mItems: MutableList<Coverable> = contents
     private var mSections = mutableListOf<CollectionViewSection>()
-
-    /**
-     * Set all indications about selection status.
-     * Requires mSelector to be properly activated or deactivated.
-     */
-    fun setAllIndications(rv: RecyclerView, section: CollectionViewSection) {
-        for (pos in 0 until mItems.size) {
-            val holder = rv.findViewHolderForAdapterPosition(pos)
-            val positionInSection = getPositionInSection(pos)
-            if (holder is CoverViewHolder) {
-                section.indicateSelection(holder, positionInSection, section.selector.selectedItemIds)
-            }
-        }
-    }
 
     /**
      * Fetch newest data and rebuild
--- app/src/main/java/iced/egret/palette/adapter/PinnedCollectionsAdapter.kt
+++ app/src/main/java/iced/egret/palette/adapter/PinnedCollectionsAdapter.kt
@@ -1,8 +1,6 @@
 package iced.egret.palette.adapter
 
-import androidx.recyclerview.widget.RecyclerView
 import iced.egret.palette.model.Collection
-import iced.egret.palette.recyclerview_component.CoverViewHolder
 import iced.egret.palette.recyclerview_component.PinnedCollectionsSection
 import iced.egret.palette.util.CollectionManager
 import io.github.luizgrp.sectionedrecyclerviewadapter.Section
@@ -11,32 +9,6 @@ class PinnedCollectionsAdapter : CoverableAdapter() {
 
     private var mCollections = CollectionManager.getCollections().toMutableList()
     private var mSections = mutableListOf<PinnedCollectionsSection>()
-
-    /**
-     * Set all indications about selectability and selection status.
-     * Requires mSelector to be properly activated or deactivated.
-     */
-    fun setAllIndications(rv: RecyclerView, section: PinnedCollectionsSection) {
-        setAllIndicateIsSelectable(rv, section)
-        for (pos in 0 until mCollections.size) {
-            val holder = rv.findViewHolderForAdapterPosition(pos)
-            if (holder is CoverViewHolder) section.indicateSelectionStatus(holder, pos, section.selector.selectedItemIds)
-        }
-    }
-
-    /**
-     * Indicate that certain covers are not selectable based on item attributes.
-     * Requires mSelector to be properly activated or deactivated.
-     */
-    fun setAllIndicateIsSelectable(rv: RecyclerView, section: PinnedCollectionsSection) {
-        for (pos in 0 until mCollections.size) {
-            val item = mCollections[pos]
-            if (!item.deletable) {
-                val holder = rv.findViewHolderForAdapterPosition(pos)
-                if (holder is CoverViewHolder) section.indicateIsSelectable(holder, item)
-            }
-        }
-    }
 
     fun updateCollections() {
 

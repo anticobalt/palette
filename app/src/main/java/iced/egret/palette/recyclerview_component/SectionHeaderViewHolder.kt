226ed611be4d92c23df124a2ea3f4fb7b9773b23
anticobalt <anticobalt@users.noreply.github.com>
2019-07-01 20:44:21 -0600

    Fix: duplicate sections created on section expansion

--- app/src/main/java/iced/egret/palette/recyclerview_component/SectionHeaderViewHolder.kt
+++ app/src/main/java/iced/egret/palette/recyclerview_component/SectionHeaderViewHolder.kt
@@ -14,4 +14,20 @@ class SectionHeaderViewHolder(view: View,
 
     val tvItem = view.findViewById<TextView>(R.id.tvCollectionViewSectionHeader)
 
+    // A hacky way to resolve sections being duplicated after CoverableItems are deselected.
+    // Straight-up using a non-expandable implementation (e.g. just a normal FlexibleViewHolder)
+    // does not work because it also duplicates sections when restoring them.
+    // At least with expandable sections (i.e. Use Case 2 from https://github.com/davideas/
+    // FlexibleAdapter/wiki/5.x-%7C-Headers-and-Sections), the duplication can be avoided
+    // until section expansion is invoked.
+
+    override fun isViewExpandableOnClick(): Boolean {
+        return false
+    }
+    override fun isViewCollapsibleOnClick(): Boolean {
+        return false
+    }
+    override fun isViewCollapsibleOnLongClick(): Boolean {
+        return false
+    }
 }

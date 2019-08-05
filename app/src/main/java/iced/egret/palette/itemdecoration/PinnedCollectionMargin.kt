7e2326e6229514b325f11f70e10f62f8e9acb185
anticobalt <anticobalt@users.noreply.github.com>
2019-08-04 20:13:28 -0600

    Fix: margin glitch when entering selection mode as pinned album

--- app/src/main/java/iced/egret/palette/itemdecoration/PinnedCollectionMargin.kt
+++ app/src/main/java/iced/egret/palette/itemdecoration/PinnedCollectionMargin.kt
@@ -6,23 +6,13 @@ import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Used to add uniform margin to PinnedCollectionItems. Assumes one column.
- * Copied from https://stackoverflow.com/a/44964515
  */
 class PinnedCollectionMargin(private val space : Int) : RecyclerView.ItemDecoration() {
 
-    override fun getItemOffsets(outRect: Rect, view: View,
-                       parent: RecyclerView, state: RecyclerView.State) {
-
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         outRect.left = space
         outRect.right = space
-        outRect.bottom = space
-
-        // Add top margin only for the first item to avoid double space between items
-        if (parent.getChildLayoutPosition(view) == 0) {
-            outRect.top = space
-        } else {
-            outRect.top = 0
-        }
+        outRect.top = space
     }
 
 }
